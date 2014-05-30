4b7561ff168b08e6e56fc19855f1ccab6b69835f
Jesse Weed <jesse@jesseweed.com>
2014-05-30 10:38:51 -0700

    update production config

--- config/environment/production.js
+++ config/environment/production.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
   var tesla = require('../../lib/tesla')(app);
 
   // global settings
-  app.site.domain = '';
+  app.site.domain = 'teslajs.com';
   app.site.environment = 'production';
   app.site.url = app.config.protocol + app.site.domain + ':'  + app.config.port + '/'; // base url
 
