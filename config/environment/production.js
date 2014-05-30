2f376c531a328097edb4242e900b870a40d0b263
Jesse Weed <jesse@jesseweed.com>
2014-05-30 10:37:09 -0700

    update env settings

--- config/environment/production.js
+++ config/environment/production.js
@@ -3,23 +3,10 @@ var os = require('os');
 
 module.exports = function (app) {
 
-  var interfaces = os.networkInterfaces();
-  var addresses = [];
-  for (var k in interfaces) {
-    for (var k2 in interfaces[k]) {
-      var address = interfaces[k][k2];
-      if (address.family === 'IPv4' && !address.internal) {
-        addresses.push(address.address);
-      }
-    }
-  }
-
   var tesla = require('../../lib/tesla')(app);
 
-  if ( typeof addresses[0] === 'undefined' ) addresses[0] = 'localhost';
-
   // global settings
-  app.site.domain = addresses[0];
+  app.site.domain = '';
   app.site.environment = 'production';
   app.site.url = app.config.protocol + app.site.domain + ':'  + app.config.port + '/'; // base url
 
